340a181c26495537ff0a3f28f4c97f7ff07d1f8a
eodo57 <efrain.ovalles@gmail.com>
2025-10-14 15:37:46 -0400

    com

--- src/core/Application.cpp
+++ src/core/Application.cpp
@@ -1,84 +1,53 @@
 #include "Application.h"
-#include "Window.h"
-#include "../renderer/VulkanRenderer.h"
-#include "../renderer/Model.h"
-#include "../scene/GameObject.h" 
 #include "Logger.h"
-#include <GLFW/glfw3.h>
+#include <iostream>
 
-Application::Application() {
-    window = std::make_unique<Window>(WIDTH, HEIGHT, "Vulkan Engine");
-    vulkanRenderer = std::make_unique<VulkanRenderer>(*window);
-
-    // Create and connect the camera controller
-    cameraController = std::make_unique<CameraController>(vulkanRenderer->getCamera());
-    cameraController->connectWindow(*window);
-
-    loadGameObjects();
+Application::Application() : window(800, 600, "Vulkan Game Engine"), renderer(window), cameraController(renderer.getCamera()) {
+    Logger::info("Application starting...");
+    gameObject.model = std::make_shared<Model>(renderer.getDevice(), "models/airboat.obj");
+    gameObject.transform.translation = {0.0f, 0.0f, -2.5f};
 }
 
-Application::~Application() {}
-
-void Application::loadGameObjects() {
-    std::shared_ptr<Model> model = std::make_shared<Model>(vulkanRenderer->getDevice(), "models/airboat.obj");
-
-    auto vase = GameObject::createGameObject();
-    vase.model = model; // This will now work
-    vase.transform.translation = {0.f, 0.f, 2.5f};
-    vase.transform.scale = {3.f, 3.f, 3.f};
-    gameObjects.push_back(std::move(vase));
+Application::~Application() {
+    Logger::info("Application shutting down...");
 }
 
 void Application::run() {
-    Logger::info("Application starting...");
-
-    while (!window->shouldClose()) {
+    while (!window.shouldClose()) {
         glfwPollEvents();
 
-                // Calculate delta time
-        float currentTime = static_cast<float>(glfwGetTime());
-        float deltaTime = currentTime - lastFrameTime;
-        lastFrameTime = currentTime;
-        int mouseState = glfwGetMouseButton(window->getGlfwWindow(), GLFW_MOUSE_BUTTON_LEFT);
+        int mouseState = glfwGetMouseButton(window.getGlfwWindow(), GLFW_MOUSE_BUTTON_LEFT);
         if (mouseState == GLFW_PRESS) {
             double xpos, ypos;
-            glfwGetCursorPos(window->getGlfwWindow(), &xpos, &ypos);
+            glfwGetCursorPos(window.getGlfwWindow(), &xpos, &ypos);
             castRayAndSelect(xpos, ypos);
         }
-        // Update the camera
-        cameraController->update(deltaTime);
-        
-        vulkanRenderer->drawFrame(gameObjects[0]);
-    }
 
-    vulkanRenderer->waitIdle();
-    Logger::info("Application shutting down.");
+        cameraController.update(0.016f);
+        renderer.drawFrame(gameObject);
+    }
+    renderer.waitIdle();
 }
 
 void Application::castRayAndSelect(double mouseX, double mouseY) {
     int width, height;
-    glfwGetWindowSize(window->getGlfwWindow(), &width, &height);
+    glfwGetWindowSize(window.getGlfwWindow(), &width, &height);
     Ray ray = renderer.getCamera().castRay(static_cast<float>(mouseX), static_cast<float>(mouseY), width, height);
 
-    // Simple intersection test (replace with a more robust one later)
     float closest_t = std::numeric_limits<float>::max();
     GameObject* hitObject = nullptr;
 
-    // This assumes you have a list of gameObjects to test against.
-    // For now, we only have one `gameObject` in the Application class.
-    // We'll expand this later.
     if (intersects(ray, gameObject)) {
          hitObject = &gameObject;
     }
 
-
     if (selectedObject) {
-        selectedObject->selected = false; // Deselect the old object
+        selectedObject->selected = false;
     }
 
     if (hitObject) {
         selectedObject = hitObject;
-        selectedObject->selected = true; // Select the new object
+        selectedObject->selected = true;
         Logger::info("Object selected!");
     } else {
         selectedObject = nullptr;
@@ -87,9 +56,7 @@ void Application::castRayAndSelect(double mouseX, double mouseY) {
 }
 
 bool Application::intersects(const Ray& ray, const GameObject& object) {
-    // A simple sphere-ray intersection test
-    // We'll assume a bounding sphere for the object for now
-    float radius = 1.0f; // A placeholder radius
+    float radius = 1.0f;
     glm::vec3 oc = ray.origin - object.transform.translation;
     float a = glm::dot(ray.direction, ray.direction);
     float b = 2.0f * glm::dot(oc, ray.direction);
--- src/core/Application.h
+++ src/core/Application.h
@@ -1,34 +1,26 @@
 #pragma once
-#include "CameraController.h"
-#include "../scene/GameObject.h"
-#include <memory>
 
-class Window;
-class VulkanRenderer;
+#include "Window.h"
+#include "../renderer/VulkanRenderer.h"
+#include "../scene/GameObject.h"
+#include "CameraController.h"
+#include "../renderer/Ray.h" // <-- Make sure this is included
+
 
 class Application {
 public:
-    static constexpr int WIDTH = 800;
-    static constexpr int HEIGHT = 600;
-
     Application();
     ~Application();
-
-    Application(const Application&) = delete;
-    Application& operator=(const Application&) = delete;
 
     void run();
 
 private:
-    void loadGameObjects();
     void castRayAndSelect(double mouseX, double mouseY);
     bool intersects(const Ray& ray, const GameObject& object);
-    std::unique_ptr<Window> window;
-    std::unique_ptr<VulkanRenderer> vulkanRenderer;
-    std::vector<GameObject> gameObjects;
-    std::unique_ptr<CameraController> cameraController;
 
+    Window window;
+    VulkanRenderer renderer;
+    GameObject gameObject;
+    CameraController cameraController;
     GameObject* selectedObject = nullptr;
-
-    float lastFrameTime = 0.f;
 };
